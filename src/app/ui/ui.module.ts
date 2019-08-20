e165312da319a033b062b8fd52858d485e85c91a
Tedy Tri Saputro <detik19@gmail.com>
2019-08-21 06:05:02 +0700

    first commit

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './components/layout/layout.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class UiModule { }
